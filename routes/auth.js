f6703762000faa10cb85b07b7a6a8858b8a68e4b
Abdeladim Choukri <choukri.abdeladim@gmail.com>
2023-02-25 17:42:15 +0100

    updated auth routes structure

--- routes/auth.js
+++ routes/auth.js
@@ -2,12 +2,10 @@ const express = require("express");
 const router = express.Router();
 const upload = require("../utils/imageUpload");
 
-const { register, login, logout } = require("../controllers/auth");
-const authMiddleware = require("../middlewares/authMiddleware");
+const { register, login } = require("../controllers/auth");
 
 //
 router.route("/register").post(upload.single("picture"), register);
 router.route("/login").post(login);
-router.route("/logout").get(authMiddleware, logout);
 
 module.exports = router;
