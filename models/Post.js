86e0cca1f8e75bda32899bc5eda465c6f5d6f95b
Abdeladim Choukri <choukri.abdeladim@gmail.com>
2023-02-28 10:14:33 +0100

    deleted Token model file and some comments

--- models/Post.js
+++ models/Post.js
@@ -34,7 +34,4 @@ const PostSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-// we did add firstName and lastName cuz the post will have user credentials
-// we did set the likes as map of booleans cuz it would be efficient to see who liked which post
-// we set the picture as pat cuz we need to get that path in order to have access to the actual file
 module.exports = mongoose.model("Post", PostSchema);
